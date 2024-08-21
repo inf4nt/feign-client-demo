bdb541d26b36c8ac1f2cc4e3459b3af556e93a5c
Lukaretskyi Maksym <com.infant@gmail.com>
2024-08-21 16:51:15 +0300

    update api call

--- client/src/main/java/com/demo/client/ClientRestController.java
+++ client/src/main/java/com/demo/client/ClientRestController.java
@@ -1,13 +1,13 @@
 package com.demo.client;
 
 import com.demo.client.api.ApiClient;
-import com.demo.client.api.ApiDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
+import java.time.Instant;
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping
@@ -17,7 +17,11 @@ public class ClientRestController {
     private ApiClient apiClient;
 
     @GetMapping
-    public Collection<ApiDTO> getAllApi() {
-        return apiClient.getAll();
+    public Object getAllApi() {
+        var apiResponse = apiClient.getAll();
+        var response = new LinkedHashMap<>();
+        response.put("client_request_timestamp", Instant.now().toEpochMilli());
+        response.put("api_resources", apiResponse);
+        return response;
     }
 }
